c6cb38ba77d3b113f9b10f0adde00b69d8dae3ec
Eric Song <nordstorm@naver.com>
2016-01-03 22:37:00 +0900

    1st release
    User can select a folder or a set of multiple files to be in list.
    File extension of jpg and png are supported by default, but an arbitrary extension is selectable as well.
    Generating file list output can be any type of file in default text encoding.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 송이네
+ * Date: 2016-01-01
+ * Time: 오후 11:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace ImageListGenerator
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.toolStripDropDownButton_LoadFileList = new System.Windows.Forms.ToolStripDropDownButton();
+			this.listFromFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.listFromFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.label_prefix = new System.Windows.Forms.Label();
+			this.label_Suffix = new System.Windows.Forms.Label();
+			this.textBox_Prefix = new System.Windows.Forms.TextBox();
+			this.textBox_Suffix = new System.Windows.Forms.TextBox();
+			this.toolStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.toolStripDropDownButton_LoadFileList});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(526, 25);
+			this.toolStrip1.TabIndex = 0;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// toolStripDropDownButton_LoadFileList
+			// 
+			this.toolStripDropDownButton_LoadFileList.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripDropDownButton_LoadFileList.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.listFromFolderToolStripMenuItem,
+									this.listFromFilesToolStripMenuItem});
+			this.toolStripDropDownButton_LoadFileList.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton_LoadFileList.Image")));
+			this.toolStripDropDownButton_LoadFileList.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripDropDownButton_LoadFileList.Name = "toolStripDropDownButton_LoadFileList";
+			this.toolStripDropDownButton_LoadFileList.Size = new System.Drawing.Size(29, 22);
+			this.toolStripDropDownButton_LoadFileList.Text = "toolStripDropDownButton1";
+			// 
+			// listFromFolderToolStripMenuItem
+			// 
+			this.listFromFolderToolStripMenuItem.Name = "listFromFolderToolStripMenuItem";
+			this.listFromFolderToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+			this.listFromFolderToolStripMenuItem.Text = "List from Folder";
+			this.listFromFolderToolStripMenuItem.Click += new System.EventHandler(this.ListFromFolderToolStripMenuItemClick);
+			// 
+			// listFromFilesToolStripMenuItem
+			// 
+			this.listFromFilesToolStripMenuItem.Name = "listFromFilesToolStripMenuItem";
+			this.listFromFilesToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+			this.listFromFilesToolStripMenuItem.Text = "List from Files";
+			// 
+			// label_prefix
+			// 
+			this.label_prefix.Location = new System.Drawing.Point(12, 26);
+			this.label_prefix.Name = "label_prefix";
+			this.label_prefix.Size = new System.Drawing.Size(51, 20);
+			this.label_prefix.TabIndex = 1;
+			this.label_prefix.Text = "Prefix:";
+			this.label_prefix.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// label_Suffix
+			// 
+			this.label_Suffix.Location = new System.Drawing.Point(12, 53);
+			this.label_Suffix.Name = "label_Suffix";
+			this.label_Suffix.Size = new System.Drawing.Size(51, 20);
+			this.label_Suffix.TabIndex = 1;
+			this.label_Suffix.Text = "Suffix:";
+			this.label_Suffix.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// textBox_Prefix
+			// 
+			this.textBox_Prefix.Location = new System.Drawing.Point(57, 25);
+			this.textBox_Prefix.Name = "textBox_Prefix";
+			this.textBox_Prefix.Size = new System.Drawing.Size(457, 21);
+			this.textBox_Prefix.TabIndex = 2;
+			// 
+			// textBox_Suffix
+			// 
+			this.textBox_Suffix.Location = new System.Drawing.Point(57, 52);
+			this.textBox_Suffix.Name = "textBox_Suffix";
+			this.textBox_Suffix.Size = new System.Drawing.Size(457, 21);
+			this.textBox_Suffix.TabIndex = 2;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(526, 246);
+			this.Controls.Add(this.textBox_Suffix);
+			this.Controls.Add(this.textBox_Prefix);
+			this.Controls.Add(this.label_Suffix);
+			this.Controls.Add(this.label_prefix);
+			this.Controls.Add(this.toolStrip1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "MainForm";
+			this.Text = "ImageListGenerator";
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.TextBox textBox_Suffix;
+		private System.Windows.Forms.TextBox textBox_Prefix;
+		private System.Windows.Forms.Label label_Suffix;
+		private System.Windows.Forms.Label label_prefix;
+		private System.Windows.Forms.ToolStripMenuItem listFromFilesToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem listFromFolderToolStripMenuItem;
+		private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton_LoadFileList;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,192 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 송이네
+ * Date: 2016-01-01
+ * Time: 오후 11:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImageListGenerator
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		enum SourceType
+		{
+			Folder,
+			Files
+		};
+		
+		private List<String> FilenameList = new List<String>();
+		private String CurrentWorkingDirectory = null;
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		/*
+		 *
+		<설명>
+		Directory.GetFiles(String)
+		 - 지정된 폴더에 있는 파일의 이름(경로 포함)을 반환한다.
+		 - 하위 폴더는 검색하지 않는다.
+		
+		Directory.GetFiles(String, String)
+		 - 지정된 폴더에서 지정된 검색 패턴과 일치하는 파일 이름(파일 경로 포함)을 반환한다.
+		 - 하위폴더는 검색하지 않는다.
+		
+		Directory.GetFiles(String, String, SearchOption)
+		 - 하위 폴더를 검색할지 여부를 나타내는 값을 사용하여 지정된 디렉터리에서 지정된 검색 패턴과 일치하는 파일 이름(파일 경로 포함)을 반환한다.
+		
+		
+		<특이사항>
+		 - 지정된 폴더에 있는 숨김 파일의 이름도 반환한다.
+		
+		
+		<예제1>
+		 - 지정된 확장자의 파일목록 가져오기
+		 --------------------------------------------------------------------------------
+		  string path = "PATH";
+		  string searchPatterns = "*.txt|*.jpg";
+		  stirng[] files = searchPatterns
+		                        .Split('|')
+		                        .SelectMany(searchPattern => Directory.GetFiles(path, searchPattern)).ToArray();
+		
+		<예제2: Directory.GetFiles()를 이용>
+		--> FullFilepath가 전달된다.
+		if (Directory.Exists(path))
+		{
+			foreach (string f in Directory.GetFiles(path))
+		    {
+				// 리스트 박스나 기타 다른 반복된 작업 수행
+			}
+		}
+		
+		<예제3: DirectoryInfo.GetFiles()를 이용>
+		--> FileInfo는 세분화된 File 정보로 제공(FileName, Extension, DirectoryName 등)
+		System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(@"d:\download");
+		
+		foreach(System.IO.FileInfo f in di.GetFiles())
+		listBox1.Items.Add(f.Name + "  " + f.Extension)
+		*/
+		public bool GenerateFileListFromFolder()
+		{
+			FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+			folderDlg.Description = "Select Folder with files to enlist";		
+			if(folderDlg.ShowDialog() != DialogResult.OK)
+			{
+				return false;
+			}
+			FilenameList.Clear();
+			
+			FileInfo[] fileInfoList = new DirectoryInfo(folderDlg.SelectedPath).GetFiles();
+			foreach(FileInfo fileInfo in fileInfoList)
+			{
+				FilenameList.Add(fileInfo.Name);
+			}
+			CurrentWorkingDirectory = folderDlg.SelectedPath;
+			return true;
+		}
+		
+		public bool GenerateFileListFromFiles()
+		{
+			OpenFileDialog fileDlg = new OpenFileDialog();
+			fileDlg.Filter = "JPG Files(*.jpg)|*.jpg|PNG Files(*.png)|*.png|All Files(*.*)|*.*";
+			fileDlg.Multiselect = true;
+			fileDlg.Title = "Select Files to enlist";
+			if(CurrentWorkingDirectory != null)
+			{
+				fileDlg.InitialDirectory = CurrentWorkingDirectory;
+			}
+			if (fileDlg.ShowDialog() != DialogResult.OK )
+			{
+				return false;
+			}
+			FilenameList.Clear();
+			
+			String[] fileNameList = fileDlg.SafeFileNames;	//exclude the directory path in filename string
+			foreach(String fileName in fileNameList)
+			{
+				FilenameList.Add(fileName);
+			}
+			CurrentWorkingDirectory = fileDlg.FileNames[0];
+			return true;
+		}
+		
+		private void GenerateFileList(SourceType sourceType)
+		{
+			bool IsFileListImported = false;
+			switch(sourceType)
+			{
+				case SourceType.Folder:
+					IsFileListImported = GenerateFileListFromFolder();
+					break;
+				case SourceType.Files:
+					IsFileListImported = GenerateFileListFromFiles();
+					break;
+				default:
+					break;
+			}
+			
+			if(IsFileListImported)
+			{
+				//String quotationMarkOpen, quotationMarkClose;
+				//GetQuotationMark(out quotationMarkOpen, out quotationMarkClose);
+				String[] filepathList = FilenameList.ToArray();
+				for(int i = 0; i<filepathList.Length; i++)
+				{
+					filepathList[i] = this.textBox_Prefix.Text + filepathList[i] + this.textBox_Suffix.Text;
+				}
+				
+				SaveOutputListFile(filepathList);
+			}
+		}
+		
+		private void SaveOutputListFile(String[] filepathList)
+		{
+			OpenFileDialog outputSaveFileDlg = new OpenFileDialog();
+			if(CurrentWorkingDirectory != null)
+			{
+				outputSaveFileDlg.InitialDirectory = CurrentWorkingDirectory;
+			}
+			outputSaveFileDlg.DefaultExt = "txt";
+			outputSaveFileDlg.CheckFileExists = false;	//new creation is allowed
+			outputSaveFileDlg.CheckPathExists = false;	//new creation is allowed
+			if(outputSaveFileDlg.ShowDialog() != DialogResult.OK)
+			{
+				return;
+			}
+
+			
+			File.WriteAllLines(outputSaveFileDlg.FileName, filepathList, Encoding.Default);
+			//File.AppendAllText(outputSaveFileDlg.FileName, filepathList, Encoding.Default);
+		}
+		
+		void ListFromFolderToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			GenerateFileList(SourceType.Folder);
+		}
+		
+		void ListFromFilesToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			GenerateFileList(SourceType.Files);
+		}
+	}
+}
